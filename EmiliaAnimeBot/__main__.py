61db061f120a3556cd2a3fd43b04fe9f327f9ab9
AASFCYBERKING <89995228+AASFCYBERKING@users.noreply.github.com>
2021-09-17 10:20:03 +0530

    Update __main__.py

--- EmiliaAnimeBot/__main__.py
+++ EmiliaAnimeBot/__main__.py
@@ -86,7 +86,7 @@ CHAT_SETTINGS = {}
 USER_SETTINGS = {}
 
 for module_name in ALL_MODULES:
-    imported_module = importlib.import_module("KURUMIBOT.modules." +
+    imported_module = importlib.import_module("EmiliaAnimeBot.modules." +
                                               module_name)
     if not hasattr(imported_module, "__mod_name__"):
         imported_module.__mod_name__ = imported_module.__name__
